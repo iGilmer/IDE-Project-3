06fd0dd42713add738719bc68c1dd5d191cc39b7
iGilmer <iGilmer85@yahoo.com>
2021-11-09 16:06:37 -0500

    Progress 3

--- src/Project3Driver.java
+++ src/Project3Driver.java
@@ -1,18 +1,28 @@
 import java.lang.reflect.Member;
+import java.util.*;
 
 public class Project3Driver
 {
+
+
     static final int memberCount = 5;
+    static int memberCounter = 0;
+    static String name;
 
     public static void main(String[] args)
     {
+        Scanner input = new Scanner(System.in);
+
         Project3Member member1 = new Project3Member();
+        Project3Member member2 = new Project3Member();
+        Project3Member member3 = new Project3Member();
+        Project3Member member4 = new Project3Member();
+        Project3Member member5 = new Project3Member();
 
-        for ( int i = 0 ; i < memberCount ; i++ )
-        {
-            System.out.println("Enter a member's name:");
-
-        }
+        System.out.println("Enter a member's name:");
+        name = input.nextLine();
+        member1.setMemberName( name );
+        memberCounter++;
 
     }
 }
--- src/Project3Member.java
+++ src/Project3Member.java
@@ -1,5 +1,9 @@
+import java.util.*;
+
 public class Project3Member
 {
+    Scanner input = new Scanner(System.in);
+
     private String memberName;
     private int memberPhone;
 
@@ -13,12 +17,12 @@ public class Project3Member
         this.memberName = memberName;
         this.memberPhone = 0;
     }
-    public Project3Member( int memberPhone)
+    public Project3Member( int memberPhone )
     {
         this.memberName = " ";
         this.memberPhone = memberPhone;
     }
-    public Project3Member(String memberName, int memberPhone)
+    public Project3Member( String memberName, int memberPhone )
     {
         this.memberName = memberName;
         this.memberPhone = memberPhone;
@@ -32,5 +36,15 @@ public class Project3Member
     public int getMemberPhone()
     {
         return memberPhone;
+    }
+
+    public void setMemberName( String memberName )
+    {
+        this.memberName = memberName;
+    }
+
+    public void setMemberPhone()
+    {
+        this.memberPhone = memberPhone;
     }
 }
